26ddf79e311d366764130fd22d7d8b687aedcbfa
nejiko96 <nejiko2006@gmail.com>
2022-08-06 22:13:25 +0900

    fix eslint error: @typescript-eslint/ban-types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,6 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
 
-  const component: DefineComponent<{}, {}, any>
+  const component: DefineComponent<Record<never, never>, Record<never, never>, any>
   export default component
 }
