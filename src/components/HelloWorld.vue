f2b735ce295a7491bf75c1358e1e6f12a6d9d0ae
nejiko96 <nejiko2006@gmail.com>
2022-08-06 21:24:15 +0900

    fix eslint error: vue/singleline-html-element-content-newline

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -37,7 +37,9 @@ const count = ref(0)
     >Volar</a>
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="read-the-docs">
+    Click on the Vite and Vue logos to learn more
+  </p>
 </template>
 
 <style scoped>
