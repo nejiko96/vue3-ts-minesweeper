c3fc6d767423812d1dccfa2108a33880c5bc80e4
nejiko96 <nejiko2006@gmail.com>
2022-08-06 21:37:09 +0900

    fix eslint error: import/no-extraneous-dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+/* eslint-enable import/no-extraneous-dependencies */
 
 // https://vitejs.dev/config/
 export default defineConfig({
