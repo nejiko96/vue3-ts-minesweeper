e99e7d43f3feec6a5d64b06b7ca36f37ddd31d06
nejiko96 <nejiko2006@gmail.com>
2022-08-06 23:04:31 +0900

    add Path Alias (@...) setting from https://miyauchi.dev/ja/posts/vite-vue3-typescript/

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 /* eslint-enable import/no-extraneous-dependencies */
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   plugins: [vue()],
 })
