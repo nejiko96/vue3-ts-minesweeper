1b45b6c465f10121f96d8b873b3caff5e8fbb603
nejiko96 <nejiko2006@gmail.com>
2022-08-22 14:49:31 +0900

    slide-in effect on custom settings

--- src/components/MsSettings.vue
+++ src/components/MsSettings.vue
@@ -220,77 +220,93 @@ const mines = computed({
           </div>
         </div>
       </div>
-      <template v-if="level === 'custom'">
-        <div class="w-full px-3 mb-6">
-          <label
-            class="block tracking-wide text-sm font-semibold mb-2"
-            for="width"
-          >
-            Width
-          </label>
-          <input
-            id="width"
-            v-model.number="width"
-            class="
-              block appearance-none w-full
-              bg-gray-200 border border-gray-200 text-gray-700
-              py-1 px-4 rounded leading-tight
-              focus:outline-none focus:bg-white focus:border-gray-500
-            "
-            type="number"
-            min="9"
-            max="30"
-            step="1"
-            placeholder="9 - 30"
-          >
-        </div>
-        <div class="w-full px-3 mb-6">
-          <label
-            class="block tracking-wide text-sm font-semibold mb-2"
-            for="height"
-          >
-            Height
-          </label>
-          <input
-            id="height"
-            v-model.number="height"
-            class="
-              block appearance-none w-full
-              bg-gray-200 border border-gray-200 text-gray-700
-              py-1 px-4 rounded leading-tight
-              focus:outline-none focus:bg-white focus:border-gray-500
-            "
-            type="number"
-            min="9"
-            max="24"
-            step="1"
-            placeholder="9 - 24"
-          >
-        </div>
-        <div class="w-full px-3 mb-6">
-          <label
-            class="block tracking-wide text-sm font-semibold mb-2"
-            for="mines"
-          >
-            Mines
-          </label>
-          <input
-            id="mines"
-            v-model.number="mines"
-            class="
-              block appearance-none w-full
-              bg-gray-200 border border-gray-200 text-gray-700
-              py-1 px-4 rounded leading-tight
-              focus:outline-none focus:bg-white focus:border-gray-500
-            "
-            type="number"
-            min="10"
-            max="999"
-            step="1"
-            placeholder="10 - 999"
-          >
-        </div>
-      </template>
+      <transition name="slide-x">
+        <template v-if="level === 'custom'">
+          <div>
+            <div class="w-full px-3 mb-6">
+              <label
+                class="block tracking-wide text-sm font-semibold mb-2"
+                for="width"
+              >
+                Width
+              </label>
+              <input
+                id="width"
+                v-model.number="width"
+                class="
+                  block appearance-none w-full
+                  bg-gray-200 border border-gray-200 text-gray-700
+                  py-1 px-4 rounded leading-tight
+                  focus:outline-none focus:bg-white focus:border-gray-500
+                "
+                type="number"
+                min="9"
+                max="30"
+                step="1"
+                placeholder="9 - 30"
+              >
+            </div>
+            <div class="w-full px-3 mb-6">
+              <label
+                class="block tracking-wide text-sm font-semibold mb-2"
+                for="height"
+              >
+                Height
+              </label>
+              <input
+                id="height"
+                v-model.number="height"
+                class="
+                  block appearance-none w-full
+                  bg-gray-200 border border-gray-200 text-gray-700
+                  py-1 px-4 rounded leading-tight
+                  focus:outline-none focus:bg-white focus:border-gray-500
+                "
+                type="number"
+                min="9"
+                max="24"
+                step="1"
+                placeholder="9 - 24"
+              >
+            </div>
+            <div class="w-full px-3 mb-6">
+              <label
+                class="block tracking-wide text-sm font-semibold mb-2"
+                for="mines"
+              >
+                Mines
+              </label>
+              <input
+                id="mines"
+                v-model.number="mines"
+                class="
+                  block appearance-none w-full
+                  bg-gray-200 border border-gray-200 text-gray-700
+                  py-1 px-4 rounded leading-tight
+                  focus:outline-none focus:bg-white focus:border-gray-500
+                "
+                type="number"
+                min="10"
+                max="999"
+                step="1"
+                placeholder="10 - 999"
+              >
+            </div>
+          </div>
+        </template>
+      </transition>
     </form>
   </div>
 </template>
+
+<style scoped>
+.slide-x-enter-active,
+.slide-x-leave-active {
+  transition: transform 0.5s ease;
+}
+
+.slide-x-enter-from,
+.slide-x-leave-to {
+  transform: translateX(100%);
+}
+</style>
