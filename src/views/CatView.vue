cbb30a399276a810b4c79a8fefbed4981e7c4819
nejiko96 <nejiko2006@gmail.com>
2022-08-23 16:38:27 +0900

    today's cat implementation

--- src/views/CatView.vue
+++ src/views/CatView.vue
@@ -1,3 +1,60 @@
+<script lang="ts">
+import { ref, onBeforeMount } from 'vue'
+
+interface CatCategory {
+  id: number;
+  name: string;
+}
+
+interface SearchCatImage {
+  breeds: string[];
+  categories: CatCategory[];
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
+
+const fetchCatImage = async (): Promise<SearchCatImage> => {
+  const res = await fetch('https://api.thecatapi.com/v1/images/search')
+  const result = await res.json()
+  return result[0]
+}
+</script>
+
+<script setup lang="ts">
+const catImageUrl = ref('')
+
+const updateCatImage = async () => {
+  const image = await fetchCatImage()
+  catImageUrl.value = image.url
+}
+
+const handleClick = updateCatImage
+
+onBeforeMount(updateCatImage)
+</script>
+
 <template>
-  <h1>Cat API Test</h1>
+  <div class="p-4">
+    <button
+      class="
+        block mx-auto
+        px-4 py-2 rounded
+        bg-red-500 hover:bg-red-600
+        text-white font-semibold
+        border border-transparent  hover:border-gray-300
+        cursor-pointer transiion duration-300
+      "
+      @click="handleClick"
+    >
+      Today's Cat
+    </button>
+    <img
+      class="mx-auto mt-4"
+      :src="catImageUrl"
+      width="500"
+      height="auto"
+    >
+  </div>
 </template>
