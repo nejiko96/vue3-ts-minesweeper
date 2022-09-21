3089340bfd4c63aaf36c0439ada96dedc13ab67d
nejiko96 <nejiko2006@gmail.com>
2022-09-21 15:34:26 +0900

    add layout component

--- src/App.vue
+++ src/App.vue
@@ -3,55 +3,34 @@
 
   import MsView from '@/views/MsView.vue'
   import CatView from '@/views/CatView.vue'
+  import AppLayout from './AppLayout.vue'
+
+  const menuItems = [
+    {
+      id: 'MS',
+      title: 'Minesweeper',
+      view: MsView,
+    },
+    {
+      id: 'CAT',
+      title: 'Cat API',
+      view: CatView,
+    },
+  ]
 </script>
 
 <script setup lang="ts">
   const currentView = shallowRef(MsView)
+
+  const handleMenuSelect = (id: string): void => {
+    currentView.value = menuItems.find((e) => e.id === id)?.view ?? MsView
+  }
 </script>
 
 <template>
-  <header>
-    <nav class="bg-teal-500 text-white">
-      <div
-        class="container mx-auto flex h-16 items-center justify-between px-2 py-4"
-      >
-        <h1 class="text-xl font-semibold">Vue3 + TypeScript demo page</h1>
-
-        <div>
-          <ul class="flex items-center justify-end pb-0">
-            <li>
-              <a
-                href="#"
-                class="rounded px-8 py-2 hover:bg-teal-600"
-                @click="currentView = MsView"
-                >Minesweeper</a
-              >
-            </li>
-            <li class="--border-none">
-              <a
-                href="#"
-                class="rounded px-8 py-2 hover:bg-teal-600"
-                @click="currentView = CatView"
-                >Cat API</a
-              >
-            </li>
-          </ul>
-        </div>
-
-        <a
-          href="https://github.com/nejiko96/vue3-ts-minesweeper"
-          target="_blank"
-          title="View source on GitHub"
-          aria-label="View source on GitHub"
-        >
-          <fa icon="fa-brands fa-github" size="2xl" />
-        </a>
-      </div>
-    </nav>
-  </header>
-  <main>
+  <AppLayout :menu-items="menuItems" @menuselect="handleMenuSelect">
     <keep-alive>
       <component :is="currentView" />
     </keep-alive>
-  </main>
+  </AppLayout>
 </template>
---
+++ src/AppLayout.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+  import { MenuItemType } from './types'
+
+  const props = defineProps<{
+    menuItems: MenuItemType[]
+  }>()
+
+  const emits = defineEmits<{
+    (e: 'menuselect', id: string): void
+  }>()
+</script>
+
+<template>
+  <header>
+    <nav class="bg-teal-500 text-white">
+      <div
+        class="container mx-auto flex h-16 items-center justify-between px-2 py-4"
+      >
+        <h1 class="text-xl font-semibold">Vue3 + TypeScript demo page</h1>
+
+        <div>
+          <ul class="flex items-center justify-end pb-0">
+            <li v-for="item in props.menuItems" :key="item.id">
+              <a
+                href="#"
+                class="rounded px-8 py-2 hover:bg-teal-600"
+                @click="() => emits('menuselect', item.id)"
+                >{{ item.title }}</a
+              >
+            </li>
+          </ul>
+        </div>
+
+        <a
+          href="https://github.com/nejiko96/vue3-ts-minesweeper"
+          target="_blank"
+          title="View source on GitHub"
+          aria-label="View source on GitHub"
+        >
+          <fa icon="fa-brands fa-github" size="2xl" />
+        </a>
+      </div>
+    </nav>
+  </header>
+  <main>
+    <slot></slot>
+  </main>
+</template>
