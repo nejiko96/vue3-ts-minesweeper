90b4aaa5aa1a321d81d22a464d7cc40d758ac28b
nejiko96 <nejiko2006@gmail.com>
2022-10-20 17:55:34 +0900

    WordleView:word selector design

--- src/views/WordleView.vue
+++ src/views/WordleView.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
-
   const chars = 'aerosunityquirk'
   const hints = '  ?  ? !  !!!!!'.split('')
 
@@ -15,33 +13,79 @@
 <template>
   <div class="p-4 text-center">
     <h1 class="text-3xl font-semibold">Wordle Helper</h1>
-    <div class="flex grow items-center justify-center">
-      <div class="grid grid-cols-5 grid-rows-6 gap-x-2 gap-y-2 py-10">
-        <template v-for="(_, k) in 30" :key="k">
-          <div
-            class="box-border inline-flex h-16 w-16 items-center justify-center uppercase text-white"
-            :class="charClass(k)"
+    <div class="--items-center flex grow justify-evenly pt-10">
+      <div class="--text-left --w-96">
+        <h3 class="mb-2 text-2xl font-semibold">
+          Select From Search Result(10)
+        </h3>
+        <ul class="mb-2 grid grid-cols-3 grid-rows-3 gap-x-2 gap-y-2">
+          <li
+            class="w-40 rounded-lg bg-sky-500 p-2 text-xl text-white hover:bg-sky-300"
           >
-            <span class="text-4xl font-bold">{{ chars[k] }}</span>
-          </div>
-        </template>
+            BENNI
+          </li>
+          <li
+            class="w-40 rounded-lg bg-sky-500 p-2 text-xl text-white hover:bg-sky-300"
+          >
+            DENIM
+          </li>
+          <li
+            class="w-40 rounded-lg bg-sky-500 p-2 text-xl text-white hover:bg-sky-300"
+          >
+            GENIC
+          </li>
+          <li
+            class="w-40 rounded-lg bg-sky-500 p-2 text-xl text-white hover:bg-sky-300"
+          >
+            GENIE
+          </li>
+        </ul>
+
+        <h3 class="mb-2 text-2xl font-semibold">Select From Suggestion</h3>
+        <ul class="mb-2 grid grid-cols-3 grid-rows-2 gap-x-2 gap-y-2">
+          <li
+            class="w-40 rounded-lg bg-pink-500 p-2 text-xl text-white hover:bg-pink-300"
+          >
+            GUIMP
+          </li>
+          <li
+            class="w-40 rounded-lg bg-pink-500 p-2 text-xl text-white hover:bg-pink-300"
+          >
+            GRAMP
+          </li>
+        </ul>
+
+        <h3 class="mb-2 text-2xl font-semibold">Edit Menu</h3>
+        <ul class="grid grid-cols-3 grid-rows-1 gap-x-2 gap-y-2">
+          <li
+            class="w-40 rounded-lg bg-slate-500 p-2 text-xl text-white hover:bg-slate-300"
+          >
+            <fa icon="fa-rotate-left" size="sm" />
+            Undo
+          </li>
+          <li
+            class="w-40 rounded-lg bg-slate-500 p-2 text-xl text-white hover:bg-slate-300"
+          >
+            <fa icon="fa-trash-can" size="sm" />
+            Clear
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <h3 class="mb-2 text-2xl font-semibold">Present/Correct/Absent</h3>
+        <div class="grid grid-cols-5 grid-rows-6 gap-x-2 gap-y-2">
+          <template v-for="(_, k) in 30" :key="k">
+            <div
+              class="box-border inline-flex h-16 w-16 items-center justify-center uppercase text-white"
+              :class="charClass(k)"
+            >
+              <span class="text-4xl font-bold">{{ chars[k] }}</span>
+            </div>
+          </template>
+        </div>
       </div>
     </div>
-
-    <a
-      href="#"
-      class="block max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow-md hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-    >
-      <h5
-        class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-      >
-        Noteworthy technology acquisitions 2021
-      </h5>
-      <p class="font-normal text-gray-700 dark:text-gray-400">
-        Here are the biggest enterprise technology acquisitions of 2021 so far,
-        in reverse chronological order.
-      </p>
-    </a>
   </div>
 </template>
 
@@ -102,12 +146,24 @@
     background-color: var(--color-present);
   }
 
+  .present:hover {
+    background-color: #fef08a;
+  }
+
   .correct {
     background-color: var(--color-correct);
   }
 
+  .correct:hover {
+    background-color: #a1e1c7;
+  }
+
   .absent {
     background-color: var(--color-absent);
+  }
+
+  .absent:hover {
+    background-color: #bcc1c4;
   }
 
   .empty {
