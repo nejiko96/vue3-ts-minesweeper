acb757155034842e1a830375d5360f368bd000cb
nejiko96 <nejiko2006@gmail.com>
2022-12-05 17:22:57 +0900

    [WIP] FlagTrainingView.vue: layout refinement

--- src/modules/national_flag/components/FlagLabel.vue
+++ src/modules/national_flag/components/FlagLabel.vue
@@ -1,5 +1,27 @@
+<script setup lang="ts">
+  withDefaults(
+    defineProps<{
+      dummy?: boolean
+    }>(),
+    {
+      dummy: false,
+    }
+  )
+
+  const emit = defineEmits(['close'])
+</script>
 <template>
-  <div class="w-[220px] rounded bg-gray-300 py-2 text-black">
+  <div
+    v-if="dummy"
+    class="w-[220px] rounded-lg border-2 border-dashed border-gray-400 bg-transparent py-1"
+  >
+    <slot></slot>
+  </div>
+  <div
+    v-else
+    class="w-[220px] rounded-lg bg-gray-300 py-2 text-black"
+    @click="emit('close')"
+  >
     <slot></slot>
   </div>
 </template>
--- src/modules/national_flag/views/FlagTrainingView.vue
+++ src/modules/national_flag/views/FlagTrainingView.vue
@@ -5,10 +5,9 @@
     Dialog,
     TransitionChild,
     DialogPanel,
-    DialogTitle,
   } from '@headlessui/vue'
   import draggable, { MoveEvent } from 'vuedraggable'
-  import { shuffle } from '@/core/utils'
+  import { sample, shuffle } from '@/core/utils'
   import {
     FlagType,
     FlagFilterType,
@@ -18,35 +17,45 @@
   } from '../models/flagsModel'
   import FlagLabel from '../components/FlagLabel.vue'
 
-  type FlagChoiceType = FlagType & {
-    choice: FlagType[]
+  type FlagAnswerType = FlagType & {
+    answer: FlagType[]
   }
 
   const trainingList = getFilterList()
 </script>
+
 <script setup lang="ts">
-  const trainingObj = ref<FlagFilterType>(getFilterList()[0])
+  const trainingObj = ref<FlagFilterType>(sample(trainingList))
 
   const org = ref<FlagType[]>([])
 
-  const shuffled = ref<FlagChoiceType[]>([])
+  const shuffled = ref<FlagAnswerType[]>([])
 
   const checkMove = (evt: MoveEvent<FlagType>): boolean => {
-    if (evt.relatedContext.list === org.value) return true
-    return evt.relatedContext.list.length === 0
+    const el = evt.relatedContext.component.$el
+    if (el.classList.contains('pool')) return true
+    if (evt.relatedContext.list.length === 0) return true
+    return false
   }
 
   const handleRestart = () => {
     org.value = getFlagList(trainingObj.value.id)
     shuffled.value = shuffle(org.value).map((o) => ({
       ...o,
-      choice: [],
+      answer: [],
     }))
+  }
+
+  const handleRemove = (flag: FlagAnswerType) => {
+    if (flag.answer[0] && flag.id !== flag.answer[0].id) {
+      const f = flag.answer.pop() as FlagType
+      org.value.push(f)
+    }
   }
 
   const isDialogOpen = ref(false)
 
-  const handleTrainingClick = (id: string) => {
+  const handleTrainingSelect = (id: string) => {
     trainingObj.value = getFilter(id)
     handleRestart()
     isDialogOpen.value = false
@@ -54,12 +63,13 @@
 
   onMounted(handleRestart)
 </script>
+
 <template>
   <div class="bg-gray-200 p-4 text-center dark:bg-gray-800">
     <h1 class="mb-4 text-3xl font-semibold">
       National Flag Training :
       <button
-        class="rounded border-2 border-transparent bg-yellow-500 px-4 py-2 text-white transition duration-300 hover:border-yellow-300 hover:bg-yellow-600"
+        class="rounded-lg border-2 border-transparent bg-orange-500 px-4 py-2 text-white transition duration-300 hover:border-orange-300 hover:bg-orange-600"
         @click="isDialogOpen = true"
       >
         {{ trainingObj.titleJa }}
@@ -74,35 +84,38 @@
               <img class="h-full w-full object-contain" :src="flag.url" />
             </div>
             <div
-              class="mx-auto h-auto w-[240px] border-2 border-yellow-300 bg-yellow-100 p-2"
+              class="mx-auto h-auto w-[240px] border-2 border-orange-300 bg-orange-100 p-2"
             >
               <draggable
-                :list="flag.choice"
+                :list="flag.answer"
                 group="flags"
                 item-key="id"
                 ghost-class="ghost"
                 :move="checkMove"
               >
                 <template #item="{ element }">
-                  <FlagLabel
+                  <FlagLabel @click="() => handleRemove(flag)"
                     >{{ element.id === flag.id ? '⭕️' : '❌' }}
                     {{ element.nameJa }}</FlagLabel
                   >
                 </template>
               </draggable>
+              <FlagLabel v-if="flag.answer.length === 0" dummy
+                >&nbsp;</FlagLabel
+              >
             </div>
           </div>
         </template>
       </div>
 
       <div
-        class="h-[150px] w-[1030px] border-2 border-yellow-300 bg-yellow-100 p-2"
+        class="min-h-[150px] w-[1030px] border-2 border-orange-300 bg-orange-100 p-2"
       >
         <draggable
           :list="org"
           group="flags"
           item-key="id"
-          class="flex flex-row flex-wrap gap-x-2 gap-y-2"
+          class="pool flex flex-row flex-wrap gap-x-2 gap-y-2"
           ghost-class="ghost"
           :move="checkMove"
         >
@@ -150,8 +163,8 @@
                       :key="tr.id"
                     >
                       <li
-                        class="mb-2 rounded bg-yellow-500 px-4 py-1 text-white"
-                        @click="() => handleTrainingClick(tr.id)"
+                        class="mb-2 rounded bg-orange-500 px-4 py-1 text-white"
+                        @click="() => handleTrainingSelect(tr.id)"
                       >
                         {{ tr.titleJa }}
                       </li>
